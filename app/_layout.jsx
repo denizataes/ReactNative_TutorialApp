e28443886e77b4df6e34e659d42daa5c66a6ffcf
denizataes <denizataes@hotmail.com>
2024-06-15 23:17:39 +0300

    auth screens implemented

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -40,6 +40,8 @@ const RootLayout = () => {
   return (
     <Stack>
         <Stack.Screen name='index' options={{headerShown: false}}></Stack.Screen>
+        <Stack.Screen name='(auth)' options={{headerShown: false}}></Stack.Screen>
+        <Stack.Screen name='(tabs)' options={{headerShown: false}}></Stack.Screen>
     </Stack>
   )
 }
---
+++ components/CustomButton.jsx
@@ -0,0 +1,21 @@
+import { TouchableOpacity, Text, Touchable } from 'react-native'
+import React from 'react'
+
+const CustomButton = ({title, handlePress, containerStyles, textStyles, isLoading}) => {
+  return (
+<TouchableOpacity 
+activeOpacity={0.7}
+onPress={handlePress}
+className={
+    `bg-secondary rounded-xl min-h-[62] justify-center items-center ${containerStyles}
+    ${isLoading ? 'opacity-50' : ''}`}
+    disabled={isLoading}
+    >
+
+        <Text className={`text-primary font-psemibold text-lg ${textStyles}`}>{title}</Text>
+
+</TouchableOpacity>
+  )
+}
+
+export default CustomButton
